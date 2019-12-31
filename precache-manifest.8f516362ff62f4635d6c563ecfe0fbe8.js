b830f5ca33573ace206459602f6866ae1c9af559
leon <li.zhiang@cgtn.com>
2019-12-31 17:53:00 +0800

    init

---
+++ precache-manifest.8f516362ff62f4635d6c563ecfe0fbe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310f0d7f96be49ee78626e454dd71cc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734f7621325fa32a7224",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0744ee92fc7ddb95f841",
+    "url": "/static/js/2.5716d249.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5716d249.chunk.js.LICENSE"
+  },
+  {
+    "revision": "734f7621325fa32a7224",
+    "url": "/static/js/main.76855ec6.chunk.js"
+  },
+  {
+    "revision": "22281826f6f5bffce8ea",
+    "url": "/static/js/runtime-main.04e23692.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
